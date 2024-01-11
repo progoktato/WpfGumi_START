fe655d8cb41a62b90d86afa91233a4cad6649726
Hagymási Gyula <oktato.ber@gmail.com>
2024-01-11 16:40:09 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGumi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Tetel> tetelek = new ObservableCollection<Tetel>();
+
+        List<String> atmerok = new List<String>() { "R10", "R12", "R13", "R14", "R15", "R16", "R17", "R18", "R19", "R20", "R21", "R22", "R23" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgTetelek.ItemsSource = tetelek;
+            FelveszDemoAdatokat();
+
+        }
+
+        private void FelveszDemoAdatokat()
+        {
+            tetelek.Add(new Tetel("nyári", 205, 55, "R16", 4));
+            tetelek.Add(new Tetel("téli", 235, 55, "R18", 8));
+            tetelek.Add(new Tetel("téli", 205, 55, "R16", 2));
+        }
+
+    }
+}
---
+++ Tetel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfGumi
+{
+    public class Tetel
+    {
+        String tipus; // Téli, nyári, négyévszakos
+        int szelesseg;
+        int magassag;
+        string atmero;
+        int darab;
+
+        public Tetel(string tipus, int szelesseg, int magassag, string atmero, int darab)
+        {
+            this.tipus = tipus;
+            this.szelesseg = szelesseg;
+            this.magassag = magassag;
+            this.atmero = atmero;
+            this.darab = darab;
+        }
+
+        public string Típus { get => tipus; set => tipus = value; }
+        public int Szélesség { get => szelesseg; set => szelesseg = value; }
+        public int Magasság { get => magassag; set => magassag = value; }
+        public string Átmérő { get => atmero; set => atmero = value; }
+        public int Darab { get => darab; set => darab = value; }
+    }
+}
